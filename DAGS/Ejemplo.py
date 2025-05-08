fbadfd4952d838de227ba15d777395187b936145
Pol Alomar Casá <polalomarcasa@macbook-pro-de-pol.home>
2025-05-08 18:22:42 +0200

    Upate docker

--- DAGS/Ejemplo.py
+++ DAGS/Ejemplo.py
@@ -5,5 +5,8 @@ from datetime import datetime
 def hello():
     print("Hola desde tu DAG de trading 🚀")
 
-with DAG("example_trading_dag", start_date=datetime(2023, 1, 1), schedule_interval="@daily", catchup=False) as dag:
+with DAG("example_trading_dag",
+        start_date=datetime(2023, 1, 1), 
+        schedule_interval="@daily", 
+        catchup=False) as dag:
     tarea = PythonOperator(task_id="saludo", python_callable=hello)
