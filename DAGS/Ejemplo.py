3d2ceb86259b3c982bbba96fc23c0ab2030f154f
Pol Alomar Casá <polalomarcasa@macbook-pro-de-pol.home>
2025-05-08 18:03:50 +0200

    Inicializar entorno Airflow con Docer para Trading

--- DAGS/Ejemplo.py
+++ DAGS/Ejemplo.py
@@ -1,3 +1,9 @@
-import pandas as pd
-from datetime import datetime
 from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("Hola desde tu DAG de trading 🚀")
+
+with DAG("example_trading_dag", start_date=datetime(2023, 1, 1), schedule_interval="@daily", catchup=False) as dag:
+    tarea = PythonOperator(task_id="saludo", python_callable=hello)
